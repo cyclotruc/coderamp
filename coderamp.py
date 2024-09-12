c53fbf0d54c8cdf193b9c9f10b4bd06cecee92bc
MrCyclopede <romain.courtois.mails@gmail.com>
2024-09-12 05:52:38 +0000

    add todos

--- coderamp.py
+++ coderamp.py
@@ -4,3 +4,7 @@ from install import setup_coderamp
 delete_all_codeboxes()
 ip = new_instance('test1')
 setup_coderamp("51.159.164.94")
+
+#TODO class for instances with uuid, ip and name
+#TODO HA queue
+#TODO instance expiration
