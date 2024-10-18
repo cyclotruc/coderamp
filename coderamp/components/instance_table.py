87444fc8ef1a3ab42da60768447f038ddea665cb
MrCyclopede <romain.courtois.mails@gmail.com>
2024-10-18 01:54:42 +0000

    rework instance table

--- coderamp/components/instance_table.py
+++ coderamp/components/instance_table.py
@@ -1,13 +1,14 @@
 import reflex as rx
-from reflex.style import color_mode
-from datetime import datetime, timedelta
+from datetime import timedelta
 from ..coderamp_lib.coderamp import Instance
+from reflex.utils.prerequisites import get_app
 import asyncio
 
 
 class InstanceTableState(rx.State):
     instances: list[dict] = []
     update: bool = False
+    time: int = 0
 
     def load_entries(self):
         instances = Instance.select().order_by(Instance.created_at.desc()).limit(10)
@@ -18,7 +19,7 @@ class InstanceTableState(rx.State):
                     "name": instance.name,
                     "age": instance.created_at + timedelta(hours=2),
                     "link": instance.public_url or "",
-                    "stopable": instance.state == "ready"
+                    "accessible": instance.state == "ready"
                     or instance.state == "allocated",
                     "state": instance.state,
                     "ip": instance.public_ip or "",
@@ -26,17 +27,30 @@ class InstanceTableState(rx.State):
                     "id": instance.get_id(),
                 }
             )
+        self.time += 1
         self.instances = updated_instances
 
-    async def stop_handler(self, id: int):
+    @rx.background
+    async def autorefresh_handler(self):
+        print("Autorefresh handler started")
+        app_object = get_app()
+        while self.router.session.client_token in app_object.app.event_namespace.token_to_sid:
+            async with self:
+                self.load_entries()
+            await asyncio.sleep(1)
+        print("Autorefresh handler stopped")
+
+    async def retire_handler(self, id: int):
         instance = Instance.get_by_id(id)
         await instance.retire()
         self.load_entries()
 
     async def delete_handler(self, id: int):
         instance = Instance.get_by_id(id)
-        await instance.retire()
-        instance.delete_instance()
+        if instance.state == "retired":
+            await instance.delete()
+        else:
+            raise ValueError("Instance is not retired")
         self.load_entries()
 
 
@@ -46,7 +60,7 @@ def instance_row(instance: dict) -> rx.Component:
         rx.table.cell(rx.moment(instance["age"], from_now=True, interval=1000)),
         rx.table.cell(
             rx.cond(
-                (instance["state"].to(str) == "ready"),
+                (instance["accessible"]),
                 rx.link(
                     rx.button("Open"),
                     href=instance["link"].to(str),
@@ -58,20 +72,15 @@ def instance_row(instance: dict) -> rx.Component:
         rx.table.cell(
             rx.hstack(
                 rx.cond(
-                    instance["stopable"],
+                    instance["state"].to(str) != "retired",
                     rx.button(
-                        "Stop",
-                        on_click=lambda: InstanceTableState.stop_handler(
+                        "Retire",
+                        on_click=lambda: InstanceTableState.retire_handler(
                             instance["id"]
                         ),
-                        color_scheme="gray",
+                        color_scheme="red",
                     ),
-                    rx.button("Stop", disabled=True, color_scheme="gray"),
-                ),
-                rx.button(
-                    "Delete",
-                    on_click=lambda: InstanceTableState.delete_handler(instance["id"]),
-                    color_scheme="red",
+                    rx.button("Retire", disabled=True, color_scheme="red"),
                 ),
             ),
         ),
@@ -137,12 +146,23 @@ def instance_row(instance: dict) -> rx.Component:
         ),
         rx.table.cell(instance["ip"]),
         rx.table.cell(rx.badge(instance["ports"])),
+        rx.table.cell(
+            rx.button(
+                rx.icon(tag="trash"),
+                on_click=lambda: InstanceTableState.delete_handler(instance["id"]),
+                color_scheme="gray",
+            ),
+        ),
+        align_items="center",  # Add this line to center items vertically
+        on_mount=InstanceTableState.autorefresh_handler,
     )
 
 
 def instance_table() -> rx.Component:
     return rx.center(
         rx.card(
+            rx.vstack(
+                rx.text(InstanceTableState.time),
             rx.table.root(
                 rx.table.header(
                     rx.table.row(
@@ -158,10 +178,17 @@ def instance_table() -> rx.Component:
                                 "Refresh", on_click=InstanceTableState.load_entries
                             )
                         ),
+                        # rx.table.column_header_cell(
+                        #     rx.button(
+                        #         "Auto-refresh",
+                        #         on_click=InstanceTableState.autorefresh_handler,
+                        #     )
+                        # ),
                     ),
                 ),
                 rx.table.body(rx.foreach(InstanceTableState.instances, instance_row)),
                 on_mount=InstanceTableState.load_entries,
+            )
             )
         )
     )
