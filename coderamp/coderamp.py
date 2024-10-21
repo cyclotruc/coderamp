620b959bc62b9d650543fc6f2c89a46be7cd066a
MrCyclopede <romain.courtois.mails@gmail.com>
2024-10-21 03:40:41 +0000

    clean

--- coderamp/coderamp.py
+++ coderamp/coderamp.py
@@ -5,24 +5,12 @@ from coderamp.coderamp_lib.coderamp import Coderamp, full_reset
 
 
 async def global_tick():
-    # full_reset()
-    # ramp1 = Coderamp.create()
-    # ramp1.configure(
-    #     "Basic",
-    #     open_file="pages/index.md",
-    #     open_folder="/root/evidence-starter",
-    #     setup_commands="",
-    #     ports="3000",
-    #     vm_type="DEV1-S",
-    #     min_instances=1,
-    # )
     try:
         print("Global tick started")
         while True:
             for coderamp in Coderamp.select().where(Coderamp.active == True):
                 await coderamp.tick()
 
-            # input()
             await asyncio.sleep(10)
 
     except asyncio.CancelledError:
